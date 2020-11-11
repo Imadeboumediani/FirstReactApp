21142cce276f4bfa35c78ccf8840064edc701274
Imad Boumediani <boumediani.pro@gmail.com>
2020-11-11 16:05:36 +0100

    Updates

---
+++ precache-manifest.c652bea39a0b180d90e6d03f9a060491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a567294348a20364f01a319e05358a",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "2ab3e0aaade790011a5f",
+    "url": "/FirstReactApp/static/css/main.1861d584.chunk.css"
+  },
+  {
+    "revision": "cabf8e95084adacc9657",
+    "url": "/FirstReactApp/static/js/2.5723f631.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/FirstReactApp/static/js/2.5723f631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab3e0aaade790011a5f",
+    "url": "/FirstReactApp/static/js/main.b215a98c.chunk.js"
+  },
+  {
+    "revision": "858cc7558245055d71bf",
+    "url": "/FirstReactApp/static/js/runtime-main.f54249ab.js"
+  }
+]);
