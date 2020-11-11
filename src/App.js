591c115dbc41984419420cf416293fec0d6a81a6
Imad Boumediani <boumediani.pro@gmail.com>
2020-11-11 16:06:54 +0100

    ajout des fichiers pour github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import  { CardList } from './components/card-list/card-list.components';
 import './App.css';
+import { Search } from './components/search/search.component';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
+class App extends Component  {
+  constructor() {
+    super();
+    this.state ={
+      monsters: [],
+      searchField:''
+    };
+
+  }
+
+  componentDidMount() {
+    fetch ('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange= (e) => {
+    this.setState({searchField :e.target.value})
+  }
+
+ render () {
+   const {monsters,searchField} = this.state;
+   const filteredMonsters =monsters.filter( monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+   return(
+     
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <h1> First React APP by I.B</h1>
+      <Search type='text' placeholder='Search users' handleChange={this.handleChange}/>
+      <Particles
+        className='particles'
+        params={{
+          "particles": {
+              "number": {
+                  "value": 200
+              },
+              "size": {
+                  "value": 2
+              },
+              "density": {
+                   "enable": true,
+                   "value_area": 500
+                   
+      }
+          },
+          "interactivity": {
+              "events": {
+                  "onhover": {
+                      "enable": true,
+                      "mode": "repulse"
+                  }
+              }
+              
+          }
+          
+      }} />  
+      <CardList monsters={filteredMonsters}  />
+     
+     
     </div>
   );
-}
 
-export default App;
+   }
+
+  }
+export default App ; 
